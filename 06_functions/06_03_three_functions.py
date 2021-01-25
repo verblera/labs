70f9d0efcd139f8508b591ef108b93e34b363e0f
Lera <werblera@gmail.com>
2021-01-25 06:36:41 +0000

    finishing functions

--- 06_functions/06_03_three_functions.py
+++ 06_functions/06_03_three_functions.py
@@ -5,3 +5,33 @@ at least one other function and use the return value to do something.
 '''
 
 
+def age_count(years):
+    # takes years, calls a function to return minutes and returns seconds
+    seconds = age_count_2(years)*60
+    return seconds
+
+
+def age_count_2(yars):
+    # takes years and returns minutes
+    minutes = yars * 365 * 24
+    return minutes
+
+
+def init(name):
+    age = int(input(f'What\'s your age, {name}? '))
+    print(f'So, you lived for {age} years, or {age_count(age)} seconds')
+    yes_or_no = input('Pretty old, huh? yes/no?')
+    i = 0
+    while yes_or_no.lower() != 'yes':
+        print(f'Don\'t agree? Let me remind you, you\'re {age_count(age)} seconds old')
+        yes_or_no = input('Pretty old, huh? yes/no?')
+        i += 1
+        if i > 4:
+            print('Hard to persuade you, huh')
+            break
+    if yes_or_no.lower() == 'yes':
+        print(f'Bye, {name}, my old friend')
+
+
+user_name = input('Hello! What\'s your name?')
+init(user_name)
