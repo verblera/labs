d863626cd0c82e96fe77f9792ea4bc00a281a9c6
Lera <werblera@gmail.com>
2021-02-01 15:06:36 +0000

    'argskwargs'

--- 16_variable_arguments/16_01_args.py
+++ 16_variable_arguments/16_01_args.py
@@ -1,4 +1,13 @@
-'''
+"""
 Write a script with a function that demonstrates the use of *args.
 
-'''
+"""
+import random
+def schedule(*args):
+    with open('schedule.txt', 'w') as promises:
+        promises.write('I promise to work out following days...')
+        words = ['for sure', 'honestly', 'definitely', 'no doubt', 'seriously']
+        for day in args:
+            promises.write(f'\n{day}, {random.choice(words)}')
+
+schedule('09.02', '11.02', '14.02', '17.02', '21.02', '01.03', '02.03')
--- 16_variable_arguments/16_02_kwargs.py
+++ 16_variable_arguments/16_02_kwargs.py
@@ -1,4 +1,12 @@
-'''
+"""
 Write a script with a function that demonstrates the use of **kwargs.
 
-'''
+"""
+
+def names(**kwargs):
+    with open('names.txt', 'w') as facebook:
+        for name, familyname in kwargs.items():
+            facebook.write(f'{name} {familyname}\n')
+
+
+names(Anna = 'Smith', James = 'Bond', Sherlock ='Holmes', Donald = 'Trump', Sebastian = 'Kurz', Lady = 'Gaga')
