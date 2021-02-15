66c5bcfb004821e41d713c1acb53537339a1966b
Lera <werblera@gmail.com>
2021-02-15 09:51:29 +0000

    adding3

---
+++ 17_decorators/3.py
@@ -0,0 +1,21 @@
+# does something
+
+def decorate(symbol):
+    def my_decorator2(func):
+        def wrapper(*args):
+            return f'{"".center(80, symbol)}\n {func(*args)}\n {"".center(80, symbol)}'
+        return wrapper
+    return my_decorator2
+
+
+@decorate('*')
+def asterix(sentence):
+    return sentence
+
+
+@decorate('%')
+def percent(a):
+    return a
+
+print(asterix(input('Type in a sentence: ')))
+print(percent(input('Type in another sentence: ')))
