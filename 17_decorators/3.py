4e245c85d0c18b136bca8a671e52ec623ed97d10
Lera <werblera@gmail.com>
2021-02-15 10:00:35 +0000

    3py-deleting spaces

--- 17_decorators/3.py
+++ 17_decorators/3.py
@@ -3,7 +3,7 @@
 def decorate(symbol):
     def my_decorator2(func):
         def wrapper(*args):
-            return f'{"".center(80, symbol)}\n {func(*args)}\n {"".center(80, symbol)}'
+            return f'{"".center(80, symbol)}\n{func(*args)}\n{"".center(80, symbol)}'
         return wrapper
     return my_decorator2
 
